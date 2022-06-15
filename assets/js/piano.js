b2d7893959b29bd56c6f7960d84622ecd64f44b9
bryanagui <bje.aguinaldo@gmail.com>
2022-06-16 01:03:47 +0800

    added lyre

--- assets/js/piano.js
+++ assets/js/piano.js
@@ -1,82 +1,219 @@
 $(document).ready(function () {
   $("#keyboard-note-a").on("click", function () {
-    const note_do = new Audio("assets/sfx/do.wav");
-    note_do.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_do = new Audio("assets/sfx/do.wav");
+      note_do.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_do2 = new Audio("assets/sfx/lyre/do2.wav");
+      lyre_do2.play();
+    }
   });
 
   $("#keyboard-note-s").on("click", function () {
-    const note_re = new Audio("assets/sfx/re.wav");
-    note_re.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_re = new Audio("assets/sfx/re.wav");
+      note_re.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_re2 = new Audio("assets/sfx/lyre/re2.wav");
+      lyre_re2.play();
+    }
   });
 
   $("#keyboard-note-d").on("click", function () {
-    const note_mi = new Audio("assets/sfx/mi.wav");
-    note_mi.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_mi = new Audio("assets/sfx/mi.wav");
+      note_mi.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_mi2 = new Audio("assets/sfx/lyre/mi2.wav");
+      lyre_mi2.play();
+    }
   });
 
   $("#keyboard-note-f").on("click", function () {
-    const note_fa = new Audio("assets/sfx/fa.wav");
-    note_fa.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_fa = new Audio("assets/sfx/fa.wav");
+      note_fa.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_fa2 = new Audio("assets/sfx/lyre/fa2.wav");
+      lyre_fa2.play();
+    }
   });
 
   $("#keyboard-note-g").on("click", function () {
-    const note_sol = new Audio("assets/sfx/sol.wav");
-    note_sol.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_sol = new Audio("assets/sfx/sol.wav");
+      note_sol.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_so2 = new Audio("assets/sfx/lyre/so2.wav");
+      lyre_so2.play();
+    }
   });
 
   $("#keyboard-note-h").on("click", function () {
-    const note_la = new Audio("assets/sfx/la.wav");
-    note_la.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_la = new Audio("assets/sfx/la.wav");
+      note_la.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_la2 = new Audio("assets/sfx/lyre/la2.wav");
+      lyre_la2.play();
+    }
   });
 
   $("#keyboard-note-j").on("click", function () {
-    const note_ti = new Audio("assets/sfx/ti.wav");
-    note_ti.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_ti = new Audio("assets/sfx/ti.wav");
+      note_ti.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_ti2 = new Audio("assets/sfx/lyre/ti2.wav");
+      lyre_ti2.play();
+    }
   });
 
   $("#keyboard-note-k").on("click", function () {
-    const note_do2 = new Audio("assets/sfx/do2.wav");
-    note_do2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_do2 = new Audio("assets/sfx/do2.wav");
+      note_do2.play();
+    }
   });
 
   $("#keyboard-note-q").on("click", function () {
-    const note_do3 = new Audio("assets/sfx/do3.wav");
-    note_do3.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_do3 = new Audio("assets/sfx/do3.wav");
+      note_do3.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_do1 = new Audio("assets/sfx/lyre/do1.wav");
+      lyre_do1.play();
+    }
   });
 
   $("#keyboard-note-w").on("click", function () {
-    const note_re2 = new Audio("assets/sfx/re2.wav");
-    note_re2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_re2 = new Audio("assets/sfx/re2.wav");
+      note_re2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_re1 = new Audio("assets/sfx/lyre/re1.wav");
+      lyre_re1.play();
+    }
   });
 
   $("#keyboard-note-e").on("click", function () {
-    const note_mi2 = new Audio("assets/sfx/mi2.wav");
-    note_mi2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_mi2 = new Audio("assets/sfx/mi2.wav");
+      note_mi2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_mi1 = new Audio("assets/sfx/lyre/mi1.wav");
+      lyre_mi1.play();
+    }
   });
 
   $("#keyboard-note-r").on("click", function () {
-    const note_fa2 = new Audio("assets/sfx/fa2.wav");
-    note_fa2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_fa2 = new Audio("assets/sfx/fa2.wav");
+      note_fa2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_fa1 = new Audio("assets/sfx/lyre/fa1.wav");
+      lyre_fa1.play();
+    }
   });
 
   $("#keyboard-note-t").on("click", function () {
-    const note_sol2 = new Audio("assets/sfx/sol2.wav");
-    note_sol2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_sol2 = new Audio("assets/sfx/sol2.wav");
+      note_sol2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_so1 = new Audio("assets/sfx/lyre/so1.wav");
+      lyre_so1.play();
+    }
   });
 
   $("#keyboard-note-y").on("click", function () {
-    const note_la2 = new Audio("assets/sfx/la2.wav");
-    note_la2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_la2 = new Audio("assets/sfx/la2.wav");
+      note_la2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_do1 = new Audio("assets/sfx/lyre/la1.wav");
+      lyre_do1.play();
+    }
   });
 
   $("#keyboard-note-u").on("click", function () {
-    const note_ti2 = new Audio("assets/sfx/ti2.wav");
-    note_ti2.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_ti2 = new Audio("assets/sfx/ti2.wav");
+      note_ti2.play();
+    }
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_ti1 = new Audio("assets/sfx/lyre/ti1.wav");
+      lyre_ti1.play();
+    }
   });
 
   $("#keyboard-note-i").on("click", function () {
-    const note_do4 = new Audio("assets/sfx/do4.wav");
-    note_do4.play();
+    if ($("#instrument").val() == "Piano") {
+      const note_do4 = new Audio("assets/sfx/do4.wav");
+      note_do4.play();
+    }
+  });
+
+  $("#keyboard-note-z").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_do4 = new Audio("assets/sfx/lyre/do3.wav");
+      lyre_do4.play();
+    }
+  });
+
+  $("#keyboard-note-x").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_re3 = new Audio("assets/sfx/lyre/re3.wav");
+      lyre_re3.play();
+    }
+  });
+
+  $("#keyboard-note-c").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_mi3 = new Audio("assets/sfx/lyre/mi3.wav");
+      lyre_mi3.play();
+    }
+  });
+
+  $("#keyboard-note-v").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_fa3 = new Audio("assets/sfx/lyre/fa3.wav");
+      lyre_fa3.play();
+    }
+  });
+
+  $("#keyboard-note-b").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_so3 = new Audio("assets/sfx/lyre/so3.wav");
+      lyre_so3.play();
+    }
+  });
+
+  $("#keyboard-note-n").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_la3 = new Audio("assets/sfx/lyre/la3.wav");
+      lyre_la3.play();
+    }
+  });
+
+  $("#keyboard-note-m").on("click", function () {
+    if ($("#instrument").val() == "Lyre") {
+      const lyre_ti3 = new Audio("assets/sfx/lyre/ti3.wav");
+      lyre_ti3.play();
+    }
   });
 
   $(document).keydown(function (e) {
@@ -127,6 +264,37 @@ $(document).ready(function () {
     }
     if (e.keyCode == 75 || e.keyCode == 104) {
       $("#keyboard-note-k").trigger("click");
+    }
+    if (e.keyCode == 90) {
+      $("#keyboard-note-z").trigger("click");
+    }
+    if (e.keyCode == 88) {
+      $("#keyboard-note-x").trigger("click");
+    }
+    if (e.keyCode == 67) {
+      $("#keyboard-note-c").trigger("click");
+    }
+    if (e.keyCode == 86) {
+      $("#keyboard-note-v").trigger("click");
+    }
+    if (e.keyCode == 66) {
+      $("#keyboard-note-b").trigger("click");
+    }
+    if (e.keyCode == 78) {
+      $("#keyboard-note-n").trigger("click");
+    }
+    if (e.keyCode == 77) {
+      $("#keyboard-note-m").trigger("click");
+    }
+  });
+
+  $("#instrument").change(function (e) {
+    if ($("#instrument").val() == "Piano") {
+      $("#piano").removeClass("hidden");
+      $("#lyre").addClass("hidden");
+    } else if ($("#instrument").val() == "Lyre") {
+      $("#lyre").removeClass("hidden");
+      $("#piano").addClass("hidden");
     }
   });
 });
